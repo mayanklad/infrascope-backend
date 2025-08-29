7e8188da997e33deedaef61245cf16300ee8eadd
mayanklad <mayanklad12@gmail.com>
2025-08-29 14:34:59 -0400

    Move basic validations from service to controller

--- src/main/java/com/infrascope/backend/service/FileService.java
+++ src/main/java/com/infrascope/backend/service/FileService.java
@@ -15,55 +15,17 @@ import java.util.Set;
 import java.util.UUID;
 
 @Service
-public class UploadService {
+public class FileService {
 
     private final Path uploadDir = Path.of("uploads");
 
-    private static final Set<String> ALLOWED_EXTENSIONS = Set.of(
-            "tf",
-            "yml",
-            "yaml",
-            "docker-compose.yml",
-            "docker-compose.yaml"
-    );
-
-    public UploadService() throws IOException {
+    public FileService() throws IOException {
         if (!Files.exists(uploadDir)) {
             Files.createDirectories(uploadDir);
         }
     }
 
-    public void validateFileType(MultipartFile file) throws IllegalArgumentException {
-        String filename = file.getOriginalFilename();
-
-        if (filename == null || filename.isBlank()) {
-            throw new IllegalArgumentException("File name is empty.");
-        }
-
-        boolean allowed = ALLOWED_EXTENSIONS.stream()
-                .anyMatch(ext -> filename.equalsIgnoreCase(ext) || filename.endsWith("." + ext));
-
-        if (!allowed) {
-            throw new IllegalArgumentException("Invalid file type: " + filename);
-        }
-    }
-
-    public ResponseEntity<?> handleFileUpload(MultipartFile file) {
-        try {
-            validateFileType(file);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity
-                    .badRequest()
-                    .body(e.getMessage());
-        }
-
-//      5 MB limit
-        if (file.getSize() > 5 * 1024 * 1024) {
-            return ResponseEntity
-                    .status(HttpStatus.PAYLOAD_TOO_LARGE)
-                    .body("File size exceeds 5MB limit.");
-        }
-
+    public ResponseEntity<String> handleFileUpload(MultipartFile file) {
 //      Sanitize filename
         String sanitizedFilename = file.getOriginalFilename().trim().replaceAll("[^a-zA-Z0-9_.-]", "_");
         String uniqueName = UUID.randomUUID() + "-" + sanitizedFilename;
@@ -82,6 +44,6 @@ public class UploadService {
 
         return ResponseEntity
                 .created(URI.create(response.getPath()))
-                .body(response);
+                .body(response.toString());
     }
 }

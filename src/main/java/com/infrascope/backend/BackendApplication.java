2ec34cb6ee90fdab0d9a9ccc5c2349949d183944
mayanklad <mayanklad12@gmail.com>
2025-10-09 00:29:30 -0400

    Add web mvc cors

--- src/main/java/com/infrascope/backend/BackendApplication.java
+++ src/main/java/com/infrascope/backend/BackendApplication.java
@@ -2,6 +2,9 @@ package com.infrascope.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +12,20 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {//@Value("${frontend.url}") String FRONTEND_URL) {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173/")//FRONTEND_URL)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+
+        };
+    }
 
 }
